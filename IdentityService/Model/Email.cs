981bf7c1d54c7f780a77412a3951fe182ce3d929
AlialhadiAbokhalil <166250920+AliAlhadiAk@users.noreply.github.com>
2024-08-21 16:07:48 +0300

    Delete IdentityService/Model/Email.cs

--- IdentityService/Model/Email.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace IdentityService.Model
-{
-    public class Email
-    {
-        public static string EmailSender = "alialhadiabokhalil@gmail.com";
-        public static string EmailSenderPass = "asp.net_c#123";
-    }
-}
