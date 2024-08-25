70b14c23ee671dde2410d9eeb9c05470098fcd26
Alialhadi <Alialhadi@ALIALHADIAK>
2024-08-25 16:35:19 +0300

    adding config

--- BasketService/Program.cs
+++ BasketService/Program.cs
@@ -6,7 +6,33 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+  services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
+        });
+        services.AddMediatR(typeof(CreateShoppingCartCommandHandler).GetTypeInfo().Assembly);
+        services.AddScoped<IBasketRepository, BasketRepository>();
+        services.AddScoped<ICorrelationIdGenerator, CorrelationIdGenerator>();
+        services.AddAutoMapper(typeof(Startup));
+        services.AddScoped<DiscountGrpcService>();
+        services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+            (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
 
+        services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+        services.AddSwaggerGen(options =>
+        {
+            options.OperationFilter<SwaggerDefaultValues>();
+        });
+        services.AddHealthChecks()
+            .AddRedis(Configuration["CacheSettings:ConnectionString"], "Redis Health", HealthStatus.Degraded);
+        services.AddMassTransit(config =>
+        {
+            config.UsingRabbitMq((ct, cfg)=>
+            {
+                cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+            });
+        });
+        services.AddMassTransitHostedService();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
