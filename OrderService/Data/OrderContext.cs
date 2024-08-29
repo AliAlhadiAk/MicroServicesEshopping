12c4159a3d2061a6bbb8ac541bb81aad4a8f4f50
Alialhadi <Alialhadi@ALIALHADIAK>
2024-08-29 18:11:04 +0300

    adding order service

---
+++ OrderService/Data/OrderContext.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit.Transports;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderService.Data
+{
+    public class OrderContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
